27d557ebbae81aaa776f82e9ec6d45e95a6040fb
Ammar_Emad <za0663008@gmail.com>
2026-03-15 15:07:30 +0200

    feat: Add `Assets` class to centralize and manage application image paths.

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -2,17 +2,9 @@
 class Assets {
   Assets._();
   
-  /// Assets for imagesAdviceIcon
-  /// assets/images/adviceIcon.png
-  static const String imagesAdviceIcon = "assets/images/adviceIcon.png";
-
   /// Assets for imagesAsrImage
   /// assets/images/AsrImage.png
   static const String imagesAsrImage = "assets/images/AsrImage.png";
-
-  /// Assets for imagesAzanImage
-  /// assets/images/AzanImage.png
-  static const String imagesAzanImage = "assets/images/AzanImage.png";
 
   /// Assets for imagesAzkarAfterPreying
   /// assets/images/azkar_after_preying.png
@@ -33,10 +25,6 @@ class Assets {
   /// Assets for imagesClockIcon
   /// assets/images/clockIcon.png
   static const String imagesClockIcon = "assets/images/clockIcon.png";
-
-  /// Assets for imagesDoaaAndAzkarImage
-  /// assets/images/doaa_and_azkar_image.png
-  static const String imagesDoaaAndAzkarImage = "assets/images/doaa_and_azkar_image.png";
 
   /// Assets for imagesDoaaHandIcon
   /// assets/images/doaaHandIcon.png
@@ -70,21 +58,9 @@ class Assets {
   /// assets/images/ishaaImage.png
   static const String imagesIshaaImage = "assets/images/ishaaImage.png";
 
-  /// Assets for imagesLanguageImage
-  /// assets/images/language_image.png
-  static const String imagesLanguageImage = "assets/images/language_image.png";
-
   /// Assets for imagesLocationIcon
   /// assets/images/locationIcon.png
   static const String imagesLocationIcon = "assets/images/locationIcon.png";
-
-  /// Assets for imagesLocationImage
-  /// assets/images/LocationImage.png
-  static const String imagesLocationImage = "assets/images/LocationImage.png";
-
-  /// Assets for imagesLocationImage2
-  /// assets/images/LocationImage_2.png
-  static const String imagesLocationImage2 = "assets/images/LocationImage_2.png";
 
   /// Assets for imagesMaghrebIcon
   /// assets/images/maghrebIcon.png
@@ -98,49 +74,17 @@ class Assets {
   /// assets/images/markImage.png
   static const String imagesMarkImage = "assets/images/markImage.png";
 
-  /// Assets for imagesMaspahaImage
-  /// assets/images/maspahaImage.png
-  static const String imagesMaspahaImage = "assets/images/maspahaImage.png";
-
   /// Assets for imagesMushafIcon
   /// assets/images/mushafIcon.png
   static const String imagesMushafIcon = "assets/images/mushafIcon.png";
-
-  /// Assets for imagesMushafImage
-  /// assets/images/mushafImage.png
-  static const String imagesMushafImage = "assets/images/mushafImage.png";
 
   /// Assets for imagesNotificationIcon
   /// assets/images/notificationIcon.png
   static const String imagesNotificationIcon = "assets/images/notificationIcon.png";
 
-  /// Assets for imagesOnboardImage1
-  /// assets/images/onboardImage_1.png
-  static const String imagesOnboardImage1 = "assets/images/onboardImage_1.png";
-
-  /// Assets for imagesOnboardImage2
-  /// assets/images/onboardImage_2.png
-  static const String imagesOnboardImage2 = "assets/images/onboardImage_2.png";
-
-  /// Assets for imagesOnboardImage3
-  /// assets/images/onboardImage_3.png
-  static const String imagesOnboardImage3 = "assets/images/onboardImage_3.png";
-
   /// Assets for imagesQeblaIcon
   /// assets/images/qeblaIcon.png
   static const String imagesQeblaIcon = "assets/images/qeblaIcon.png";
-
-  /// Assets for imagesQeblaImage
-  /// assets/images/qeblaImage.png
-  static const String imagesQeblaImage = "assets/images/qeblaImage.png";
-
-  /// Assets for imagesQuranLearningImage
-  /// assets/images/quranLearningImage.png
-  static const String imagesQuranLearningImage = "assets/images/quranLearningImage.png";
-
-  /// Assets for imagesRememberImage
-  /// assets/images/rememberImage.png
-  static const String imagesRememberImage = "assets/images/rememberImage.png";
 
   /// Assets for imagesSearchIcon
   /// assets/images/searchIcon.png
