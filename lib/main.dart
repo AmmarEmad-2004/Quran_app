212d7adbed74b940a8951776dd27347dfed83553
Ammar_Emad <za0663008@gmail.com>
2026-03-15 15:16:55 +0200

    feat: Initialize the basic Flutter application structure with main entry point, root app widget, and splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/quran_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuranApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const Scaffold(),
-    );
-  }
-}
