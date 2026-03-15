537134fa0b45e496dce4bb15ff44cfb64c951954
Ammar_Emad <za0663008@gmail.com>
2026-03-15 15:17:07 +0200

    feat: Introduce initial application setup with `QuranApp`, Arabic localization, and a basic `SplashScreen`.

---
+++ lib/quran_app.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class QuranApp extends StatelessWidget {
+  const QuranApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('ar'),
+      supportedLocales: const [Locale('ar')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: const Scaffold(),
+    );
+  }
+}
