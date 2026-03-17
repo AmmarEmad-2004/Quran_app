7cddc4faf8ad84ac47296f12a34f3edbbdb825f6
Ammar_Emad <za0663008@gmail.com>
2026-03-17 23:00:29 +0200

    feat: add initial onboarding screen structure including body with background and custom page view.

---
+++ lib/features/onboarding/widgets/onboarding_screen_body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/core/constants/app_images.dart';
+
+class OnBoardingScreenBody extends StatelessWidget {
+  const OnBoardingScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.sizeOf(context).height,
+      width: MediaQuery.sizeOf(context).width,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppImages.onboardingBackground),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(children: [
+          
+        ],
+      ),
+    );
+  }
+}
