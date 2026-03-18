733de9e027a4f9bb8e35fd0f9d3bf35e688db039
Ammar_Emad <za0663008@gmail.com>
2026-03-18 21:38:43 +0200

    feat: implement onboarding screen with page view, navigation, and individual item display.

---
+++ lib/features/onboarding/ui/widgets/location_screen_body.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:quran_app/core/constants/app_images.dart';
+import 'package:quran_app/core/routing/app_routers.dart';
+import 'package:quran_app/core/theme/app_colors.dart';
+import 'package:quran_app/core/theme/app_styles.dart';
+import 'package:quran_app/core/widgets/custom_gradient_button.dart';
+import 'package:quran_app/core/widgets/custom_text_button.dart';
+import 'package:quran_app/features/onboarding/ui/widgets/location_info_list.dart';
+
+class LocationScreenBody extends StatelessWidget {
+  const LocationScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 10,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              AppImages.locationIconWithShadow,
+              fit: BoxFit.contain,
+              width: 140,
+              height: 140,
+            ),
+            Text(
+              "تحديد الموقع",
+              style: AppStyles.mediumCairo24(context).copyWith(fontSize: 30),
+            ),
+            Text(
+              "نحتاج إلى موقعك لحساب أوقات الصلاة الدقيقة في منطقتك وإظهار المساجد القريبة منك",
+              textAlign: TextAlign.center,
+              style: AppStyles.regularCairo16(
+                context,
+              ).copyWith(color: AppColors.darkGrey),
+            ),
+            SizedBox(height: 20),
+            Expanded(child: LocationInfoList()),
+            CustomGradientButton(
+              text: "السماح بالوصول للموقع",
+              fontSize: 18,
+              height: 50,
+              image: AppImages.locationIcon,
+              onTap: () {
+                GoRouter.of(context).go(AppRouters.quran);
+              },
+            ),
+            CustomTextButton(
+              text: "تخطي الأن",
+              onPressed: () {
+                GoRouter.of(context).go(AppRouters.home);
+              },
+              textStyle: AppStyles.mediumCairo14(
+                context,
+              ).copyWith(color: AppColors.darkGrey),
+            ),
+            Row(
+              spacing: 10,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(AppImages.protectIconGrey),
+                Text(
+                  "يمكنك تغيير هذا الإعداد لاحقًا من الإعدادات",
+                  style: AppStyles.regularCairo12(context),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
