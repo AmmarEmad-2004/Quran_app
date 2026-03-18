266a1cb0dc9bdfeae90f83901832048042b6f0e9
Ammar_Emad <za0663008@gmail.com>
2026-03-18 21:10:03 +0200

    feat: Implement onboarding screen with custom navigation, next/previous buttons, and page view.

--- lib/features/onboarding/widgets/custom_nav_button.dart
+++ lib/features/onboarding/widgets/custom_nav_button.dart
@@ -3,34 +3,48 @@ import 'package:quran_app/core/theme/app_colors.dart';
 import 'package:quran_app/core/theme/app_styles.dart';
 
 class CustomNavButton extends StatelessWidget {
-  const CustomNavButton({super.key});
-
+  const CustomNavButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    required this.gradientColor1,
+    required this.gradientColor2,
+  });
+  final String text;
+  final VoidCallback onTap;
+  final Color gradientColor1;
+  final Color gradientColor2;
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [AppColors.lightGreen, AppColors.darkGreen],
+            colors: [gradientColor1, gradientColor2],
           ),
           borderRadius: BorderRadius.circular(33554400),
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
           child: Row(
             spacing: 3,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "التالي",
+                text,
                 style: AppStyles.mediumCairo16(
                   context,
                 ).copyWith(color: AppColors.kWhite),
               ),
-              Icon(Icons.arrow_forward_ios, color: AppColors.kWhite, size: 16),
+              if (text == "التالي")
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: AppColors.kWhite,
+                  size: 16,
+                ),
             ],
           ),
         ),
