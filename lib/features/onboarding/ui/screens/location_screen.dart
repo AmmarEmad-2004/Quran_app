946df10cb1c76b1e1cb6829f42cc94553ba13ae4
Ammar_Emad <za0663008@gmail.com>
2026-03-18 21:20:58 +0200

    feat: Implement the initial onboarding flow, including language and location selection screens and their associated UI widgets.

---
+++ lib/features/onboarding/ui/screens/location_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/features/onboarding/ui/widgets/location_screen_body.dart';
+
+class LocationScreen extends StatelessWidget {
+  const LocationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LocationScreenBody();
+  }
+}
