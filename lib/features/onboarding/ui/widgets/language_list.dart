8f0a5a87fe00d731c5e5527ea840557cc7a00897
Ammar_Emad <za0663008@gmail.com>
2026-03-18 21:37:29 +0200

    feat: implement initial onboarding flow with dedicated screens and widgets for language and location selection.

---
+++ lib/features/onboarding/ui/widgets/language_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/core/constants/app_images.dart';
+import 'package:quran_app/features/onboarding/data/models/language_model.dart';
+import 'package:quran_app/features/onboarding/ui/widgets/language_item.dart';
+
+class LanguageList extends StatefulWidget {
+  const LanguageList({super.key});
+
+  @override
+  State<LanguageList> createState() => _LanguageListState();
+}
+
+class _LanguageListState extends State<LanguageList> {
+  int selectedLang = 0;
+  final List<LanguageModel> languages = [
+    LanguageModel(
+      image: AppImages.soudiFlag,
+      title: "العربية",
+      subTitle: "Arabic",
+    ),
+    LanguageModel(
+      image: AppImages.usFlag,
+      title: "English",
+      subTitle: "English",
+    ),
+    LanguageModel(image: AppImages.arduFlag, title: "اردو", subTitle: "Urdo"),
+    LanguageModel(
+      image: AppImages.frFlag,
+      title: "Français",
+      subTitle: "French",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+       physics: NeverScrollableScrollPhysics(),
+      itemCount: languages.length,
+      itemBuilder: (context, index) {
+        return LanguageItem(
+          languageModel: languages[index],
+          isSelected: selectedLang == index,
+          onTap: () {
+            setState(() {
+              selectedLang = index;
+            });
+          },
+        );
+      },
+    );
+  }
+}
