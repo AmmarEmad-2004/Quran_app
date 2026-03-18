b7bea7817b3e8b7cc815d54d6c8d97e5f1c7540a
Ammar_Emad <za0663008@gmail.com>
2026-03-18 17:03:24 +0200

    feat: Implement onboarding UI components and configure GoRouter for application navigation.

---
+++ lib/features/onboarding/widgets/dots_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/features/onboarding/widgets/custom_dot_indicator.dart';
+
+class DotsIndicator extends StatelessWidget {
+  const DotsIndicator({super.key, required this.currentPage});
+  final int currentPage;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 10,
+      children: List.generate(
+        3,
+        (index) => CustomDotIndicator(isActive: index == currentPage),
+      ),
+    );
+  }
+}
