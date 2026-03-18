3886a5fada741881ec4808210c8c3496f793d68b
Ammar_Emad <za0663008@gmail.com>
2026-03-18 21:10:38 +0200

    feat: create OnBoardingScreenBody widget to manage onboarding UI with page view, dots indicator, and navigation.

--- lib/features/onboarding/widgets/onboarding_screen_body.dart
+++ lib/features/onboarding/widgets/onboarding_screen_body.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/core/constants/app_images.dart';
-import 'package:quran_app/features/onboarding/widgets/custom_nav_button.dart';
+import 'package:quran_app/features/onboarding/widgets/custom_nav_onborading.dart';
 import 'package:quran_app/features/onboarding/widgets/custom_onboarding_page_view.dart';
 import 'package:quran_app/features/onboarding/widgets/dots_indicator.dart';
 
-class OnBoardingScreenBody extends StatelessWidget {
+class OnBoardingScreenBody extends StatefulWidget {
   const OnBoardingScreenBody({super.key});
+
+  @override
+  State<OnBoardingScreenBody> createState() => _OnBoardingScreenBodyState();
+}
+
+class _OnBoardingScreenBodyState extends State<OnBoardingScreenBody> {
+  int currentPage = 0;
+  late PageController pageController;
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: currentPage);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +35,34 @@ class OnBoardingScreenBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Spacer(flex: 1),
-          CustomOnBoardingPageView(),
-
-          DotsIndicator(currentPage: 0),
+          CustomOnBoardingPageView(
+            pageController: pageController,
+            onPageChanged: (page) => setState(() => currentPage = page),
+          ),
+          DotsIndicator(currentPage: currentPage),
           Spacer(flex: 1),
-          CustomNavButton(),
+          Container(
+            height: 1,
+            color: Color(0xffeaf0eb),
+            width: double.infinity,
+          ),
+          CustomNavOnborading(
+            currentPage: currentPage,
+            nextOnTap: () {
+              pageController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
+            previousOnTap: () {
+              pageController.previousPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
+          ),
         ],
       ),
     );
   }
 }
-
-
