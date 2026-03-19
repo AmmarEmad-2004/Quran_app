d4426b2379c0664a7254d661c5c872d58f568b19
Ammar_Emad <za0663008@gmail.com>
2026-03-19 17:17:18 +0200

    feat: Implement initial UI and core components for Quran, Prayer, Home, and Onboarding features.

--- lib/core/theme/app_dimensions.dart
+++ lib/core/theme/app_dimensions.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppDimensions {
-  static double scale(BuildContext context) {
-    double width = MediaQuery.sizeOf(context).width;
+  static const double designWidth = 430.0;
 
-    if (width < 600) {
-      return width / 400;
-    } else if (width < 1024) {
-      return width / 800;
-    } else {
-      return width / 1000;
-    }
+  static double getWidth(BuildContext context) {
+    return MediaQuery.sizeOf(context).width;
   }
 
   static double responsive(BuildContext context, double value) {
-    return value * scale(context);
+    final screenWidth = getWidth(context);
+    return (value / designWidth) * screenWidth;
   }
 }
