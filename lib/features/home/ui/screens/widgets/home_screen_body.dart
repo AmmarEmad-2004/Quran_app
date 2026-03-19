d09764bf16657419b355166feee22b38fdffb37c
Ammar_Emad <za0663008@gmail.com>
2026-03-20 00:50:23 +0200

    feat: Implement home screen body with next prayer time card and category list.

--- lib/features/home/ui/screens/widgets/home_screen_body.dart
+++ lib/features/home/ui/screens/widgets/home_screen_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/features/home/ui/screens/widgets/aya_of_today.dart';
+import 'package:quran_app/features/home/ui/screens/widgets/category_card.dart';
 import 'package:quran_app/features/home/ui/screens/widgets/custom_home_appbar.dart';
 import 'package:quran_app/features/home/ui/screens/widgets/custom_home_background.dart';
 import 'package:quran_app/features/home/ui/screens/widgets/next_prayer_card.dart';
@@ -8,31 +10,51 @@ class HomeScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bgHeight = MediaQuery.sizeOf(context).height * 0.35;
-    return Column(
-      children: [
-        SizedBox(
-          height: bgHeight,
-          child: Stack(
-            children: [
-              CustomHomeBackground(),
-              Positioned(
-                left: 20,
-                right: 20,
-                child: Column(
-                  children: [
-                    CustomHomeAppBar(),
-                    SizedBox(height: 24),
-                    NextPrayerCard(),
-                  ],
+    final bgHeight = MediaQuery.sizeOf(context).height * 0.5;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: bgHeight,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                CustomHomeBackground(),
+                Positioned(
+                  left: 20,
+                  right: 20,
+                  child: Column(
+                    children: [
+                      CustomHomeAppBar(),
+                      SizedBox(height: 24),
+                      NextPrayerCard(),
+                      SizedBox(height: 30),
+                      AyaOfToday(),
+                      SizedBox(height: 20),
+                      CategoryCardList(),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
 
+class CategoryCardList extends StatelessWidget {
+  const CategoryCardList({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(children: [CategoryCard(), CategoryCard()]),
+        Row(children: [CategoryCard(), CategoryCard()]),
+        Row(children: [CategoryCard(), CategoryCard()]),
+      ],
+    );
+  }
+}
