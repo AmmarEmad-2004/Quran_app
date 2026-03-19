12512c3fbe4aba38fc572f09a2825168fbdfd025
Ammar_Emad <za0663008@gmail.com>
2026-03-19 15:55:16 +0200

    feat: Initialize the `QuranApp` with basic routing, theming, localization, and introduce the `CustomHomeBackground` widget.

---
+++ lib/features/home/ui/screens/widgets/custom_home_background.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:quran_app/core/constants/app_images.dart';
+
+class CustomHomeBackground extends StatelessWidget {
+  const CustomHomeBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(48),
+          bottomRight: Radius.circular(48),
+        ),
+        image: DecorationImage(
+          image: AssetImage(AppImages.appBarBackground),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
