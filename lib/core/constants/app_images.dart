dfa70bb66d23c4d6cdcceeba8aa8d87461fdc96b
Ammar_Emad <za0663008@gmail.com>
2026-03-19 17:54:32 +0200

    feat: Add onboarding language selection UI, home screen next prayer card, prayer time list, and related data models and image assets.

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,54 +1,54 @@
 class AppImages {
   AppImages._();
 
-  static const String asr = "assets/images/AsrImage.png";
-  static const String azkarAfterPreying =
-      "assets/images/azkar_after_preying.png";
-  static const String azkarNoum = "assets/images/azkar_noum.png";
-  static const String azkarSabah = "assets/images/azkar_sabah.png";
-  static const String bookIcon = "assets/images/bookIcon.png";
-  static const String clockIcon = "assets/images/clockIcon.png";
-  static const String doaaHandIcon = "assets/images/doaaHandIcon.png";
-  static const String fajrIcon = "assets/images/fajrIcon.png";
-  static const String fajr = "assets/images/fajrImage.png";
-  static const String globalIcon = "assets/images/GlobalIcon.png";
-  static const String handIcon = "assets/images/handIcon.png";
-  static const String headphoneIcon = "assets/images/headphoneIcon.png";
-  static const String ishaaIcon = "assets/images/ishaaIcon.png";
-  static const String ishaa = "assets/images/ishaaImage.png";
-  static const String locationIcon = "assets/images/locationIcon.png";
-  static const String maghrebIcon = "assets/images/maghrebIcon.png";
-  static const String maghreb = "assets/images/maghrebImager.png";
-  static const String mark = "assets/images/markImage.png";
-  static const String mushafIcon = "assets/images/mushafIcon.png";
-  static const String notificationIcon = "assets/images/notificationIcon.png";
-  static const String onboardingBackground =
-      "assets/images/OnboardingBackground.png";
-  static const String qeblaIcon = "assets/images/qeblaIcon.png";
-  static const String searchIcon = "assets/images/searchIcon.png";
-  static const String sebhaIcon = "assets/images/sebhaIcon.png";
-  static const String shrouqIcon = "assets/images/shrouqIcon.png";
-  static const String shrouq = "assets/images/shrouqImage.png";
-  static const String splashBackground = "assets/images/SplashBackground.png";
+  // ================= Splash Screen =================
   static const String splash = "assets/images/splash_image.png";
-  static const String star = "assets/images/starImage.png";
-  static const String surahIcon = "assets/images/surahIcon.png";
-  static const String zuhrIandAsrcon = "assets/images/zuhrIandAsrcon.png";
-  static const String starSuraNumber = "assets/images/starSuraNumber.png";
-  static const String lightGreenCircle = "assets/images/lightGreencircle.png";
-  static const String soudiFlag = "assets/images/soudiFlag.png";
-  static const String usFlag = "assets/images/🇺🇸Flag.png";
-  static const String arduFlag = "assets/images/arduFlag.png";
-  static const String frFlag = "assets/images/frFlag.png";
-  static const String locationIconWithShadow =
-      "assets/images/locationIconWithShadow.png";
+  static const String splashBackground = "assets/images/SplashBackground.png";
+
+  // ================= Onboarding Screen =================
+  static const String onboardingBackground = "assets/images/OnboardingBackground.png";
+  static const String globalIcon = "assets/images/GlobalIcon.png";
+  static const String locationIconWithShadow = "assets/images/locationIconWithShadow.png";
   static const String locationIconGreen = "assets/images/locationIconGreen.png";
-  static const String sendIcon = "assets/images/sendIcon.png";
+  static const String mushaf1 = "assets/images/mushaf1.png";
+  static const String clock1 = "assets/images/clock1.png";
+  static const String fav1 = "assets/images/fav1.png";
+
+  // ================= Core / General / Shared =================
+  static const String appBarBackground = "assets/images/appbarBackground.png";
+  static const String notificationIcon = "assets/images/notificationIcon.png";
+  static const String searchIcon = "assets/images/searchIcon.png";
+  static const String locationIcon = "assets/images/locationIcon.png";
+
+  // ================= Home & Prayer Times =================
+  static const String card = "assets/images/Card.png";
+  static const String fajrIcon = "assets/images/fajrIcon.png";
+  static const String shrouqIcon = "assets/images/shrouqIcon.png";
+  static const String zuhrIandAsrcon = "assets/images/zuhrIandAsrcon.png";
+  static const String maghrebIcon = "assets/images/maghrebIcon.png";
+  static const String ishaaIcon = "assets/images/ishaaIcon.png";
+  static const String clockIcon = "assets/images/clockIcon.png";
+
+  // ================= Quran Screen =================
+  static const String surahIcon = "assets/images/surahIcon.png";
+  static const String star = "assets/images/starImage.png";
+  static const String mark = "assets/images/markImage.png";
+  static const String starSuraNumber = "assets/images/starSuraNumber.png";
+  static const String mushafIcon = "assets/images/mushafIcon.png";
+  static const String headphoneIcon = "assets/images/headphoneIcon.png";
+
+  // ================= Azkar & Doaa =================
+  static const String azkarSabah = "assets/images/azkar_sabah.png";
+  static const String azkarNoum = "assets/images/azkar_noum.png";
+  static const String azkarAfterPreying = "assets/images/azkar_after_preying.png";
+  static const String doaaHandIcon = "assets/images/doaaHandIcon.png";
+  static const String handIcon = "assets/images/handIcon.png";
   static const String protectIcon = "assets/images/protectIcon.png";
   static const String protectIconGrey = "assets/images/protectIconGrey.png";
-  static const String mushafIcon1 = "assets/images/mushaf1.png";
-  static const String clockIcon1 = "assets/images/clock1.png";
-  static const String fav1 = "assets/images/fav1.png";
-  static const String appBarBackground = "assets/images/appbarBackground.png";
-  static const String card = "assets/images/Card.png";
+
+  // ================= Qebla & Sebha & Nav =================
+  static const String qeblaIcon = "assets/images/qeblaIcon.png";
+  static const String sebhaIcon = "assets/images/sebhaIcon.png";
+  static const String bookIcon = "assets/images/bookIcon.png";
+  static const String sendIcon = "assets/images/sendIcon.png";
 }
