478cfba025bce76250fae2e695fd2521b11fe33e
Ammar_Emad <za0663008@gmail.com>
2026-03-20 00:51:00 +0200

    feat: add NextPrayerCard widget to display upcoming prayer information

--- lib/features/home/ui/screens/widgets/next_prayer_card.dart
+++ lib/features/home/ui/screens/widgets/next_prayer_card.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/core/helpers/app_padding.dart';
 import 'package:quran_app/core/theme/app_colors.dart';
+import 'package:quran_app/features/home/ui/screens/widgets/next_prayer_card_list.dart';
 import 'package:quran_app/features/home/ui/screens/widgets/next_prayer_card_progress_indicator.dart';
 import 'package:quran_app/features/home/ui/screens/widgets/next_prayer_card_time.dart';
-import 'package:quran_app/core/helpers/app_padding.dart';
 
 class NextPrayerCard extends StatelessWidget {
   const NextPrayerCard({super.key});
@@ -18,11 +19,14 @@ class NextPrayerCard extends StatelessWidget {
         padding: EdgeInsets.all(AppPadding.p20(context)),
         child: Column(
           spacing: 40,
-          children: [NextPrayerCardTime(), NextPrayerCardProgressIndicator()],
+          children: [
+            NextPrayerCardTime(),
+            NextPrayerCardProgressIndicator(),
+            NextPrayerCardList(),
+          ],
         ),
       ),
     );
   }
 }
-
 
