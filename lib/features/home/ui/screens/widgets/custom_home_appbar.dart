e92bb9aedbd5a09c8ea29a483c22fdf630611a25
Ammar_Emad <za0663008@gmail.com>
2026-03-19 15:54:50 +0200

    feat : create custom home app bar

---
+++ lib/features/home/ui/screens/widgets/custom_home_appbar.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+import 'package:quran_app/core/theme/app_colors.dart';
+import 'package:quran_app/core/theme/app_styles.dart';
+
+class CustomHomeAppBar extends StatelessWidget {
+  const CustomHomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "المصحف الشريف",
+              style: AppStyles.mediumCairo30(
+                context,
+              ).copyWith(color: AppColors.kWhite),
+            ),
+            Text(
+              "السلام عليكم ورحمة الله",
+              style: AppStyles.regularCairo14(
+                context,
+              ).copyWith(color: AppColors.kWhite),
+            ),
+          ],
+        ),
+
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.dark_mode_outlined, color: AppColors.kWhite),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.settings, color: AppColors.kWhite),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
