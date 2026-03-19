5f586afb5ac88df0d1e5930214e5b2b6422e285b
Ammar_Emad <za0663008@gmail.com>
2026-03-20 00:48:23 +0200

    feat: Implement the home screen UI including prayer time cards, category cards, and custom backgrounds.

---
+++ lib/features/home/ui/screens/widgets/category_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/core/constants/app_images.dart';
+import 'package:quran_app/core/helpers/app_padding.dart';
+import 'package:quran_app/core/theme/app_colors.dart';
+import 'package:quran_app/core/theme/app_styles.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.kWhite,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: AppPadding.p20(context),
+          bottom: AppPadding.p20(context),
+          right: AppPadding.p20(context),
+          left: AppPadding.p20(context),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(AppPadding.p14(context)),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [AppColors.lightGreen, AppColors.darkGreen],
+                ),
+              ),
+              child: Image.asset(AppImages.mushafIcon),
+            ),
+            Text('المصحف', style: AppStyles.mediumCairo18(context)),
+            Text(
+              'قراءة القرآن الكريم',
+              style: AppStyles.regularCairo12(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
