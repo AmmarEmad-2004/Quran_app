7452bc82af1ccfdd3d553488d9c6c42fa2a92038
Ammar_Emad <za0663008@gmail.com>
2026-03-21 20:17:16 +0200

    feat: add `CategoryCard` and `CategoryCardList` widgets for home screen categories.

--- lib/features/home/ui/screens/widgets/category_card.dart
+++ lib/features/home/ui/screens/widgets/category_card.dart
@@ -5,38 +5,45 @@ import 'package:quran_app/core/theme/app_styles.dart';
 import 'package:quran_app/features/home/data/models/category_model.dart';
 
 class CategoryCard extends StatelessWidget {
-  const CategoryCard({super.key, required this.category});
+  const CategoryCard({
+    super.key,
+    required this.category,
+  });
   final CategoryModel category;
+  
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.kWhite,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: Padding(
-        padding: EdgeInsets.only(
-          top: AppPadding.p20(context),
-          right: AppPadding.p20(context),
-          left: AppPadding.p20(context),
-          bottom: AppPadding.p20(context),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.all(AppPadding.p14(context)),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(24),
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: category.gradientColors,
+    return InkWell(
+      onTap: category.onTap,
+      child: Card(
+        color: AppColors.kWhite,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: AppPadding.p20(context),
+            right: AppPadding.p20(context),
+            left: AppPadding.p20(context),
+            bottom: AppPadding.p20(context),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.all(AppPadding.p14(context)),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: category.gradientColors,
+                  ),
                 ),
+                child: Image.asset(category.image),
               ),
-              child: Image.asset(category.image),
-            ),
-            Text(category.title, style: AppStyles.mediumCairo18(context)),
-            Text(category.subTitle, style: AppStyles.regularCairo12(context)),
-          ],
+              Text(category.title, style: AppStyles.mediumCairo18(context)),
+              Text(category.subTitle, style: AppStyles.regularCairo12(context)),
+            ],
+          ),
         ),
       ),
     );
