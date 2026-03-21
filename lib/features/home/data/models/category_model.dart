da41224bc7414239cf704deec365b5d2f031f5d2
Ammar_Emad <za0663008@gmail.com>
2026-03-21 20:10:45 +0200

    feat: Implement home screen layout with dynamic app bar, scrollable content, and category display.

--- lib/features/home/data/models/category_model.dart
+++ lib/features/home/data/models/category_model.dart
@@ -4,13 +4,11 @@ class CategoryModel {
   final String title;
   final String subTitle;
   final String image;
-  final Color gradientColor1;
-  final Color gradientColor2;
+  final List<Color> gradientColors;
   const CategoryModel({
     required this.title,
     required this.subTitle,
     required this.image,
-    required this.gradientColor1,
-    required this.gradientColor2,
+    required this.gradientColors,
   });
 }
