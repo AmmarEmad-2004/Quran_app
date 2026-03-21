b9f128032f904dc3b1c393fb28a3f24a54220c6e
Ammar_Emad <za0663008@gmail.com>
2026-03-21 20:10:58 +0200

    feat: implement home screen UI with scrollable body, animated app bar, and category cards.

--- lib/features/home/ui/screens/widgets/category_card_list.dart
+++ lib/features/home/ui/screens/widgets/category_card_list.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/core/constants/app_images.dart';
+import 'package:quran_app/core/theme/app_colors.dart';
+import 'package:quran_app/features/home/data/models/category_model.dart';
 import 'package:quran_app/features/home/ui/screens/widgets/category_card.dart';
 
 class CategoryCardList extends StatelessWidget {
@@ -6,20 +9,88 @@ class CategoryCardList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      padding: EdgeInsets.zero,
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 16,
-        childAspectRatio: 0.85, // Adjust for CategoryCard contents
-      ),
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        return const CategoryCard();
-      },
+    return Column(
+      children: [
+        GridView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12,
+            childAspectRatio: 1.12, // Adjust for CategoryCard contents
+          ),
+          itemCount: 6,
+          itemBuilder: (context, index) {
+            return CategoryCard(category: categories[index]);
+          },
+        ),
+        SizedBox(height: 12),
+        SizedBox(
+          width: double.infinity,
+          child: CategoryCard(
+            category: CategoryModel(
+              title: 'المسبحة',
+              subTitle: 'عداد التسبيح',
+              image: AppImages.sebhaIcon,
+              gradientColors: [AppColors.lightGreen, AppColors.darkGreen],
+            ),
+          ),
+        ),
+        SizedBox(height: 12),
+        SizedBox(
+          width: double.infinity,
+          child: CategoryCard(
+            category: CategoryModel(
+              title: 'اتجاه القبلة',
+              subTitle: 'تحديد اتجاه القبلة',
+              image: AppImages.qeblaIcon,
+              gradientColors: [AppColors.lightGold, AppColors.darkGold],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
+
+const List<CategoryModel> categories = [
+  CategoryModel(
+    title: 'المصحف',
+    subTitle: 'قراءة القرآن الكريم',
+    image: AppImages.mushafIcon,
+    gradientColors: [AppColors.lightGreen, AppColors.darkGreen],
+  ),
+  CategoryModel(
+    title: 'الأذان',
+    subTitle: 'مواقيت الصلاة',
+    image: AppImages.clockIcon,
+    gradientColors: [AppColors.lightGold, AppColors.darkGold],
+  ),
+  CategoryModel(
+    title: 'الأدعية والأذكار',
+    subTitle: 'حصن المسلم',
+    image: AppImages.handIcon,
+    gradientColors: [AppColors.lightGreen, AppColors.darkGreen],
+  ),
+  CategoryModel(
+    title: 'التذكيرات',
+    subTitle: 'تنبيهات يومية',
+    image: AppImages.notificationIcon,
+    gradientColors: [AppColors.lightGold, AppColors.darkGold],
+  ),
+  CategoryModel(
+    title: 'تعليم القرآن',
+    subTitle: 'دروس وتلاوات',
+    image: AppImages.headphoneIcon,
+    gradientColors: [AppColors.lightGreen, AppColors.darkGreen],
+  ),
+
+  CategoryModel(
+    title: 'المساجد',
+    subTitle: 'أقرب المساجد ',
+    image: AppImages.locationIcon,
+    gradientColors: [AppColors.lightGold, AppColors.darkGold],
+  ),
+];
