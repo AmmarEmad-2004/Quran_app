ed71c84a091459cee654da75a128dbd342b72a06
Ammar_Emad <za0663008@gmail.com>
2026-03-22 23:44:55 +0200

    feat: add initial setting screen with custom app bar and card widgets.

--- lib/features/setting/screens/setting_screen.dart
+++ lib/features/setting/screens/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/core/theme/app_colors.dart';
 import 'package:quran_app/features/setting/widgets/custom_setting_app_bar.dart';
 
 class SettingScreen extends StatelessWidget {
@@ -7,5 +8,17 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Column(children: [CustomSettingAppBar()]));
+  }
+}
+
+
+class CustomSettingCard extends StatelessWidget {
+  const CustomSettingCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.kWhite,
+    );
   }
 }
