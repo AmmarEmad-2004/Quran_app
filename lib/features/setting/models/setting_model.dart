93982fb10bbd340ca8a34518c611b8054195ac93
Ammar_Emad <za0663008@gmail.com>
2026-03-22 23:38:58 +0200

    feat: add initial setting screen UI and its corresponding data model.

---
+++ lib/features/setting/models/setting_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SettingModel {
+  final String title;
+  final String subTitle;
+  final IconData icon;
+
+ const SettingModel({
+    required this.title,
+    required this.subTitle,
+    required this.icon,
+  });
+}
