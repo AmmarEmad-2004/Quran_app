d2b7a9dc5b1abdbf2aa34abfdb25db7d8067f5ae
Ammar_Emad <za0663008@gmail.com>
2026-03-22 23:55:26 +0200

    feat: add initial setting screen with global settings for dark mode and notifications.

--- lib/features/setting/screens/setting_screen.dart
+++ lib/features/setting/screens/setting_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/core/theme/app_colors.dart';
+import 'package:quran_app/core/theme/app_styles.dart';
 import 'package:quran_app/features/setting/models/setting_model.dart';
 import 'package:quran_app/features/setting/widgets/custom_setting_app_bar.dart';
 import 'package:quran_app/features/setting/widgets/setting_item.dart';
@@ -10,57 +11,56 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [CustomSettingAppBar(), CustomSettingCard()]),
-    );
-  }
-}
-
-class CustomSettingCard extends StatelessWidget {
-  const CustomSettingCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.kWhite,
-      child: Column(
+      body: Column(
         children: [
-          SettingItem(
-            settingModel: SettingModel(
-              title: "الوضع الليلي",
-              subTitle: "تبديل بين الوضع الفاتح والداكن",
-              icon: Icons.dark_mode_outlined,
-            ),
-            trailing: Switch(value: false, onChanged: (value) {}),
-          ),
-          SettingItem(
-            settingModel: SettingModel(
-              title: "الإشعارات",
-              subTitle: "إدارة التنبيهات والإشعارات",
-              icon: Icons.notifications_none_outlined,
-            ),
-            trailing: Switch(value: false, onChanged: (value) {}),
-          ),
+          CustomSettingAppBar(),
+          SizedBox(height: 20),
+          GlobalSetting(),
+          SizedBox(height: 20),
         ],
       ),
     );
   }
 }
 
-const List<SettingModel> settingItems = [
-  SettingModel(
-    title: "الوضع الليلي",
-    subTitle: "تبديل بين الوضع الفاتح والداكن",
-    icon: Icons.dark_mode,
-  ),
-  SettingModel(
-    title: "الصوت",
-    subTitle: "الشيخ مشاري العفاسي",
-    icon: Icons.volume_up,
-  ),
-  SettingModel(title: "الخط", subTitle: "الخط العادي", icon: Icons.format_size),
-  SettingModel(
-    title: "الخلفية",
-    subTitle: "الخلفية الافتراضية",
-    icon: Icons.image,
-  ),
-];
+class GlobalSetting extends StatelessWidget {
+  const GlobalSetting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("الإعدادات العامة", style: AppStyles.mediumCairo12(context)),
+        Card(
+          color: AppColors.kWhite,
+          child: Column(
+            children: [
+              SettingItem(
+                settingModel: SettingModel(
+                  title: "الوضع الليلي",
+                  subTitle: "تبديل بين الوضع الفاتح والداكن",
+                  icon: Icons.dark_mode_outlined,
+                ),
+                trailing: Switch(value: false, onChanged: (value) {}),
+              ),
+              Container(
+                height: 1,
+                width: double.infinity,
+                color: Color(0xffe2f0ec),
+              ),
+              SettingItem(
+                settingModel: SettingModel(
+                  title: "الإشعارات",
+                  subTitle: "إدارة التنبيهات والإشعارات",
+                  icon: Icons.notifications_none_outlined,
+                ),
+                trailing: Switch(value: false, onChanged: (value) {}),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
