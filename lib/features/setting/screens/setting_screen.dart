8a6e2f373072586c40aca1a03a3a2ae6f4e0968e
Ammar_Emad <za0663008@gmail.com>
2026-03-22 23:51:30 +0200

    feat: implement initial settings screen and its reusable item widget.

--- lib/features/setting/screens/setting_screen.dart
+++ lib/features/setting/screens/setting_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:quran_app/core/theme/app_colors.dart';
+import 'package:quran_app/features/setting/models/setting_model.dart';
 import 'package:quran_app/features/setting/widgets/custom_setting_app_bar.dart';
+import 'package:quran_app/features/setting/widgets/setting_item.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [CustomSettingAppBar()]));
+    return Scaffold(
+      body: Column(children: [CustomSettingAppBar(), CustomSettingCard()]),
+    );
   }
 }
-
 
 class CustomSettingCard extends StatelessWidget {
   const CustomSettingCard({super.key});
@@ -19,6 +22,45 @@ class CustomSettingCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color: AppColors.kWhite,
+      child: Column(
+        children: [
+          SettingItem(
+            settingModel: SettingModel(
+              title: "الوضع الليلي",
+              subTitle: "تبديل بين الوضع الفاتح والداكن",
+              icon: Icons.dark_mode_outlined,
+            ),
+            trailing: Switch(value: false, onChanged: (value) {}),
+          ),
+          SettingItem(
+            settingModel: SettingModel(
+              title: "الإشعارات",
+              subTitle: "إدارة التنبيهات والإشعارات",
+              icon: Icons.notifications_none_outlined,
+            ),
+            trailing: Switch(value: false, onChanged: (value) {}),
+          ),
+        ],
+      ),
     );
   }
 }
+
+const List<SettingModel> settingItems = [
+  SettingModel(
+    title: "الوضع الليلي",
+    subTitle: "تبديل بين الوضع الفاتح والداكن",
+    icon: Icons.dark_mode,
+  ),
+  SettingModel(
+    title: "الصوت",
+    subTitle: "الشيخ مشاري العفاسي",
+    icon: Icons.volume_up,
+  ),
+  SettingModel(title: "الخط", subTitle: "الخط العادي", icon: Icons.format_size),
+  SettingModel(
+    title: "الخلفية",
+    subTitle: "الخلفية الافتراضية",
+    icon: Icons.image,
+  ),
+];
