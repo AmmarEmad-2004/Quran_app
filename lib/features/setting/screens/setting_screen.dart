269929ac789f0adee319e246aac71f06c93bb47c
Ammar_Emad <za0663008@gmail.com>
2026-03-22 23:29:47 +0200

    feat: implement initial settings screen with a custom app bar

--- lib/features/setting/screens/setting_screen.dart
+++ lib/features/setting/screens/setting_screen.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:quran_app/core/constants/app_images.dart';
-import 'package:quran_app/core/helpers/app_padding.dart';
-import 'package:quran_app/core/theme/app_colors.dart';
-import 'package:quran_app/core/theme/app_styles.dart';
+import 'package:quran_app/features/setting/widgets/custom_setting_app_bar.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
@@ -11,51 +7,5 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Column(children: [CustomSettingAppBar()]));
-  }
-}
-
-class CustomSettingAppBar extends StatelessWidget {
-  const CustomSettingAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(AppPadding.p24(context)),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(AppImages.appBarBackground),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          IconButton(
-            onPressed: () {
-              GoRouter.of(context).pop();
-            },
-            icon: Icon(Icons.arrow_back_outlined, color: AppColors.kWhite),
-          ),
-          SizedBox(width: 16),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "الإعدادات",
-                style: AppStyles.mediumCairo24(
-                  context,
-                ).copyWith(color: AppColors.kWhite),
-              ),
-              Text(
-                "تخصيص تجربتك",
-                style: AppStyles.regularCairo14(
-                  context,
-                ).copyWith(color: AppColors.kWhite),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
   }
 }
