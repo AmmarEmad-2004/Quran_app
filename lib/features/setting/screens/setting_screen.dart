a652963ff54953a726dab55b03fe011fa8d77d3d
Ammar_Emad <za0663008@gmail.com>
2026-03-23 22:19:45 +0200

    feat: Implement the initial settings screen and its custom app bar.

--- lib/features/setting/screens/setting_screen.dart
+++ lib/features/setting/screens/setting_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/core/theme/app_colors.dart';
-import 'package:quran_app/core/theme/app_styles.dart';
-import 'package:quran_app/features/setting/models/setting_model.dart';
+import 'package:quran_app/features/setting/widgets/about_app.dart';
 import 'package:quran_app/features/setting/widgets/custom_setting_app_bar.dart';
-import 'package:quran_app/features/setting/widgets/setting_item.dart';
+import 'package:quran_app/features/setting/widgets/global_setting.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
@@ -17,52 +15,7 @@ class SettingScreen extends StatelessWidget {
           SizedBox(height: 20),
           GlobalSetting(),
           SizedBox(height: 20),
-        ],
-      ),
-    );
-  }
-}
-
-class GlobalSetting extends StatelessWidget {
-  const GlobalSetting({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("الإعدادات العامة", style: AppStyles.mediumCairo12(context)),
-          SizedBox(height: 12),
-          Card(
-            color: AppColors.kWhite,
-            child: Column(
-              children: [
-                SettingItem(
-                  settingModel: SettingModel(
-                    title: "الوضع الليلي",
-                    subTitle: "تبديل بين الوضع الفاتح والداكن",
-                    icon: Icons.dark_mode_outlined,
-                  ),
-                  trailing: Switch(value: false, onChanged: (value) {}),
-                ),
-                Container(
-                  height: 1,
-                  width: double.infinity,
-                  color: Color(0xffe2f0ec),
-                ),
-                SettingItem(
-                  settingModel: SettingModel(
-                    title: "الإشعارات",
-                    subTitle: "إدارة التنبيهات والإشعارات",
-                    icon: Icons.notifications_none_outlined,
-                  ),
-                  trailing: Switch(value: false, onChanged: (value) {}),
-                ),
-              ],
-            ),
-          ),
+          AboutApp(),
         ],
       ),
     );
