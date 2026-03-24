02662603788013ad46c979eeb94c2363f6e7ec5f
Ammar_Emad <za0663008@gmail.com>
2026-03-24 21:41:24 +0200

    feat: add AzkarScreen with a CustomAzkarAppBar.

--- lib/features/azkar/ui/screens/azkar_screen.dart
+++ lib/features/azkar/ui/screens/azkar_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/features/azkar/ui/screens/widgets/custom_azkar_app_bar.dart';
 
 class AzkarScreen extends StatelessWidget {
   const AzkarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Azkar Screen"));
+    return Scaffold(body: Column(children: [CustomAzkarAppBar()]));
   }
 }
