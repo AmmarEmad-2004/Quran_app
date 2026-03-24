423bf0cfeacd815a5f83f244c6687075b953f4f1
Ammar_Emad <za0663008@gmail.com>
2026-03-24 22:24:27 +0200

    feat: Add initial Azkar screen with app bar and a grid of Azkar items.

--- lib/features/azkar/ui/screens/azkar_screen.dart
+++ lib/features/azkar/ui/screens/azkar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/features/azkar/ui/screens/widgets/azkar_item.dart';
 import 'package:quran_app/features/azkar/ui/screens/widgets/custom_azkar_app_bar.dart';
 
 class AzkarScreen extends StatelessWidget {
@@ -6,6 +7,35 @@ class AzkarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [CustomAzkarAppBar()]));
+    return Scaffold(body: Column(children: [CustomAzkarAppBar(), AzkarList()]));
+  }
+}
+
+class AzkarList extends StatelessWidget {
+  const AzkarList({super.key});
+  final List<Widget> azkar = const [
+    AzkarItem(),
+    AzkarItem(),
+    AzkarItem(),
+    AzkarItem(),
+    AzkarItem(),
+    AzkarItem(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+      ),
+      itemCount: azkar.length,
+      itemBuilder: (context, index) {
+        return azkar[index];
+      },
+    );
   }
 }
