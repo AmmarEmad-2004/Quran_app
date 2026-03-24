0e524ca418d2b234faac6e1cd66ee2e080ad25a3
Ammar_Emad <za0663008@gmail.com>
2026-03-24 22:35:28 +0200

    feat : create azkar list

--- lib/features/azkar/ui/screens/azkar_screen.dart
+++ lib/features/azkar/ui/screens/azkar_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/core/helpers/app_padding.dart';
-import 'package:quran_app/features/azkar/ui/screens/widgets/azkar_item.dart';
+import 'package:quran_app/features/azkar/ui/screens/widgets/azkar_list.dart';
 import 'package:quran_app/features/azkar/ui/screens/widgets/custom_azkar_app_bar.dart';
 
 class AzkarScreen extends StatelessWidget {
@@ -20,35 +19,4 @@ class AzkarScreen extends StatelessWidget {
   }
 }
 
-class AzkarList extends StatelessWidget {
-  const AzkarList({super.key});
-  final List<Widget> azkar = const [
-    AzkarItem(),
-    AzkarItem(),
-    AzkarItem(),
-    AzkarItem(),
-    AzkarItem(),
-    AzkarItem(),
-  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: AppPadding.p20(context)),
-      child: GridView.builder(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-          childAspectRatio: 1.2,
-        ),
-        itemCount: azkar.length,
-        itemBuilder: (context, index) {
-          return azkar[index];
-        },
-      ),
-    );
-  }
-}
