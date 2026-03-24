ac33837bbcf1b2ad8cb5de5be5adb9e9a1518cd6
Ammar_Emad <za0663008@gmail.com>
2026-03-24 21:49:29 +0200

    feat: Implement custom app bar for Azkar feature and introduce core application styles.

--- lib/features/azkar/ui/screens/widgets/custom_azkar_app_bar.dart
+++ lib/features/azkar/ui/screens/widgets/custom_azkar_app_bar.dart
@@ -27,39 +27,76 @@ class CustomAzkarAppBar extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Column(
         children: [
-          IconButton(
-            onPressed: () {
-              GoRouter.of(context).pop();
-            },
-            icon: Icon(
-              Icons.arrow_back_rounded,
-              color: AppColors.kWhite,
-              size: 16,
-            ),
-          ),
-          SizedBox(width: 16),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text(
-                "الأدعية والأذكار",
-                style: AppStyles.mediumCairo24(
-                  context,
-                ).copyWith(color: AppColors.kWhite),
+              IconButton(
+                onPressed: () {
+                  GoRouter.of(context).pop();
+                },
+                icon: Icon(
+                  Icons.arrow_back_rounded,
+                  color: AppColors.kWhite,
+                  size: 16,
+                ),
               ),
-              Text(
-                "تخصيص تجربتك",
-                style: AppStyles.regularCairo14(
-                  context,
-                ).copyWith(color: AppColors.kWhite),
+              SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "الأدعية والأذكار",
+                    style: AppStyles.mediumCairo24(
+                      context,
+                    ).copyWith(color: AppColors.kWhite),
+                  ),
+                  Text(
+                    "تخصيص تجربتك",
+                    style: AppStyles.regularCairo14(
+                      context,
+                    ).copyWith(color: AppColors.kWhite),
+                  ),
+                ],
               ),
+              Spacer(),
+              Image.asset(AppImages.doaaHandIcon),
             ],
           ),
-          Spacer(),
-          Image.asset(AppImages.doaaHandIcon),
+          AzkarCounterItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class AzkarCounterItem extends StatelessWidget {
+  const AzkarCounterItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 12, left: 12, right: 12, bottom: 12),
+      decoration: ShapeDecoration(
+        color: Colors.white.withValues(alpha: 0.10000000149011612),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      ),
+
+      child: Column(
+        children: [
+          Text(
+            "1",
+            style: AppStyles.regularCairo24(
+              context,
+            ).copyWith(color: AppColors.kWhite),
+          ),
+          Text(
+            "مكتملة اليوم",
+            style: AppStyles.regularCairo12(
+              context,
+            ).copyWith(color: AppColors.kWhite),
+          ),
         ],
       ),
     );
