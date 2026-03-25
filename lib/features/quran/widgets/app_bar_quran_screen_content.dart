f4f7453226e5b7e7db8c0586924e1cab95ed6820
Ammar_Emad <za0663008@gmail.com>
2026-03-25 22:00:12 +0200

    feat: Add custom app bar content and the main body layout for the Quran screen.

--- lib/features/quran/widgets/app_bar_quran_screen_content.dart
+++ lib/features/quran/widgets/app_bar_quran_screen_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/core/constants/app_images.dart';
 import 'package:quran_app/core/theme/app_colors.dart';
 import 'package:quran_app/core/theme/app_styles.dart';
 import 'package:quran_app/core/widgets/custom_text_feild.dart';
@@ -9,42 +10,56 @@ class AppBarQuranScreenContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 16,
-      children: [
-        Row(
-          children: [
-            Icon(Icons.arrow_back, color: Colors.white, size: 16),
-            Spacer(),
-            Column(
-              spacing: 4,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'القرآن الكريم',
-                  style: AppStyles.mediumCairo24(
-                    context,
-                  ).copyWith(color: AppColors.kWhite),
-                ),
-                Text(
-                  '114 سورة',
-                  style: AppStyles.regularCairo14(
-                    context,
-                  ).copyWith(color: AppColors.kWhite.withValues(alpha: 0.8)),
-                ),
-              ],
-            ),
-            Spacer(),
-          ],
+    return Container(
+      padding: EdgeInsets.only(
+        top: AppPadding.p20(context),
+        left: AppPadding.p20(context),
+        right: AppPadding.p20(context),
+        bottom: AppPadding.p32(context),
+      ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppImages.appBarBackground),
+          fit: BoxFit.cover,
         ),
-        CustomTextField(
-          hint: 'ابحث عن سورة...',
-          icon: Icon(
-            Icons.search,
-            color: AppColors.kWhite.withValues(alpha: 0.6),
+      ),
+      child: Column(
+        spacing: 16,
+        children: [
+          Row(
+            children: [
+              Icon(Icons.arrow_back, color: Colors.white, size: 16),
+              Spacer(),
+              Column(
+                spacing: 4,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'القرآن الكريم',
+                    style: AppStyles.mediumCairo24(
+                      context,
+                    ).copyWith(color: AppColors.kWhite),
+                  ),
+                  Text(
+                    '114 سورة',
+                    style: AppStyles.regularCairo14(
+                      context,
+                    ).copyWith(color: AppColors.kWhite.withValues(alpha: 0.8)),
+                  ),
+                ],
+              ),
+              Spacer(),
+            ],
           ),
-        ),
-      ],
+          CustomTextField(
+            hint: 'ابحث عن سورة...',
+            icon: Icon(
+              Icons.search,
+              color: AppColors.kWhite.withValues(alpha: 0.6),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
