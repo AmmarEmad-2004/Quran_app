5333c312662978db3c042e52cc5508fe1575eeb8
Ammar_Emad <za0663008@gmail.com>
2026-03-25 22:03:43 +0200

    feat: create `QuranScreenBody` widget to structure the main Quran screen UI.

--- lib/features/quran/widgets/quran_screen_body.dart
+++ lib/features/quran/widgets/quran_screen_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/core/widgets/gradient_appbar.dart';
 import 'package:quran_app/features/quran/widgets/app_bar_quran_screen_content.dart';
 import 'package:quran_app/features/quran/widgets/filter_chip_row.dart';
 import 'package:quran_app/features/quran/widgets/status_card_row.dart';
