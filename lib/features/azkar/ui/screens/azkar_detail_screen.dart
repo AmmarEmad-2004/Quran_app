030b9a707a0ead8cac67f48820896c68d4613fed
Mariam Ibrahim <neamamohammed649@gmail.com>
2026-04-12 16:24:39 +0200

    initialize azkarDetailsScreen & create customAppBar

---
+++ lib/features/azkar/ui/screens/azkar_detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/features/azkar/ui/screens/widgets/azkar_detail_screen_body.dart';
+
+class AzkarDetailScreen extends StatelessWidget {
+  const AzkarDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AzkarDetailScreenBody(),
+    );
+  }
+}
---
+++ lib/features/azkar/ui/screens/widgets/azkar_detail_screen_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/features/azkar/ui/screens/widgets/custom_azkar_app_bar.dart';
+
+class AzkarDetailScreenBody extends StatelessWidget {
+  const AzkarDetailScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(child: const Column(
+      children: [
+        CustomAzkarAppBar(),
+      ],
+    ));
+  }
+}
