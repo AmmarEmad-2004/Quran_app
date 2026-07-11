cd99c170e1fbfe6fdaa58bf7f2562be81714777a
Mariam Ibrahim <neamamohammed649@gmail.com>
2026-07-11 22:44:51 +0300

    init hive flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:quran_app/core/constants/key_constants.dart';
 import 'package:quran_app/core/helpers/di.dart';
+import 'package:quran_app/features/quran/data/models/ayah_model.dart';
+import 'package:quran_app/features/quran/data/models/surah_model.dart';
 import 'package:quran_app/quran_app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+   await Hive.initFlutter();
+  Hive.registerAdapter(SurahModelAdapter());
+  Hive.registerAdapter(AyahModelAdapter());
+  Hive.openBox<SurahModel>(KeyConstants.surahBoxName);
+  Hive.openBox<AyahModel>(KeyConstants.ayahBoxName);
+
   setupLocator();
   runApp(const QuranApp());
 }
