7bb2005dd58352be912f331948ca49bdb8daa253
Mariam Ibrahim <neamamohammed649@gmail.com>
2026-07-11 23:51:50 +0300

    refactore di file

--- lib/core/helpers/di.dart
+++ lib/core/helpers/di.dart
@@ -3,12 +3,31 @@ import 'package:get_it/get_it.dart';
 import 'package:quran_app/core/networking/api_service.dart';
 import 'package:quran_app/features/azkar/data/repos/azkar_repo.dart';
 import 'package:quran_app/features/azkar/data/repos/azkar_repo_impl.dart';
+import 'package:quran_app/features/quran/data/dataSources/quran_local_data_source.dart';
+import 'package:quran_app/features/quran/data/dataSources/quran_remote_data_source.dart';
 import 'package:quran_app/features/quran/data/repos/quran_repo.dart';
 import 'package:quran_app/features/quran/data/repos/quran_repo_impl.dart';
 
 final GetIt getIt = GetIt.instance;
 void setupLocator() {
 getIt.registerSingleton<ApiService>(ApiService(Dio()));
-getIt.registerLazySingleton<AzkarRepo>(() => AzkarRepoImpl());
-getIt.registerLazySingleton<QuranRepo>(() => QuranRepoImpl(getIt()));
+
+ // Data Sources
+  getIt.registerLazySingleton<QuranRemoteDataSource>(
+    () => QuranRemoteDataSourceImpl(getIt<ApiService>()),
+  );
+  getIt.registerLazySingleton<QuranLocalDataSource>(
+    () => QuranLocalDataSourceImpl(),
+  );
+
+ // Repositories
+  getIt.registerLazySingleton<QuranRepo>(
+    () => QuranRepoImpl(
+      getIt<QuranRemoteDataSource>(),
+      getIt<QuranLocalDataSource>(),
+    ),
+  );
+  getIt.registerLazySingleton<AzkarRepo>(
+    () => AzkarRepoImpl(),
+  );
 }
--- lib/features/quran/data/repos/quran_repo_impl.dart
+++ lib/features/quran/data/repos/quran_repo_impl.dart
@@ -1,5 +1,58 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:quran_app/core/errors/failure.dart';
+import 'package:quran_app/core/errors/server_failuer.dart';
+import 'package:quran_app/features/quran/data/dataSources/quran_local_data_source.dart';
+import 'package:quran_app/features/quran/data/dataSources/quran_remote_data_source.dart';
+import 'package:quran_app/features/quran/data/models/ayah_model.dart';
+import 'package:quran_app/features/quran/data/models/surah_model.dart';
+import 'package:quran_app/features/quran/data/repos/quran_repo.dart';
+
+class QuranRepoImpl implements QuranRepo {
+  final QuranRemoteDataSource quranRemoteDataSource;
+  final QuranLocalDataSource quranLocalDataSource;
+
+  QuranRepoImpl(this.quranRemoteDataSource, this.quranLocalDataSource);
+  @override
+  Future<Either<Failure, List<SurahModel>>> getAllSurahs() async {
+    try {
+      List<SurahModel> surahList;
+      surahList = quranLocalDataSource.getAllSurahs();
+      if (surahList.isNotEmpty) return Right(surahList);
+      surahList = await quranRemoteDataSource.getAllSurahs();
+      return Right(surahList);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailer.fromDioException(e));
+      }
+      return left(ServerFailer(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<AyahModel>>> getAllAyahsById(
+    int surahNumber,
+  ) async {
+    try {
+      List<AyahModel> ayahList;
+      ayahList = quranLocalDataSource.getAllAyahsById(surahNumber);
+      if (ayahList.isNotEmpty) return Right(ayahList);
+      ayahList = await quranRemoteDataSource.getAllAyahsById(surahNumber);
+      return Right(ayahList);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailer.fromDioException(e));
+      }
+      return left(ServerFailer(e.toString()));
+    }
+  }
+}
+
+
+
+/*
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:quran_app/core/constants/key_constants.dart';
 import 'package:quran_app/core/errors/failure.dart';
 import 'package:quran_app/core/errors/server_failuer.dart';
@@ -15,10 +68,11 @@ class QuranRepoImpl implements QuranRepo {
   @override
   Future<Either<Failure, List<SurahModel>>> getAllSurahs() async {
     try {
-      var response = await apiService.get(endPoint: KeyConstants.suraEndPoint);
-      final surahList = (response['data'] as List)
-          .map((surah) => SurahModel.fromJson(surah))
-          .toList();
+      // var response = await apiService.get(endPoint: KeyConstants.suraEndPoint);
+      // final surahList = (response['data'] as List)
+      //     .map((surah) => SurahModel.fromJson(surah))
+      //     .toList();
+     
       return Right(surahList);
     } on Exception catch (e) {
       if (e is DioException) {
@@ -33,12 +87,12 @@ class QuranRepoImpl implements QuranRepo {
     int surahNumber,
   ) async {
     try {
-      var response = await apiService.get(
-        endPoint: '${KeyConstants.suraEndPoint}/$surahNumber',
-      );
-      final ayahList = (response['data']['ayahs'] as List)
-          .map((ayah) => AyahModel.fromJson(ayah, surahNumber))
-          .toList();
+      // var response = await apiService.get(
+      //   endPoint: '${KeyConstants.suraEndPoint}/$surahNumber',
+      // );
+      // final ayahList = (response['data']['ayahs'] as List)
+      //     .map((ayah) => AyahModel.fromJson(ayah, surahNumber))
+      //     .toList();
       return Right(ayahList);
     } on Exception catch (e) {
       if (e is DioException) {
@@ -48,3 +102,5 @@ class QuranRepoImpl implements QuranRepo {
     }
   }
 }
+
+ */
