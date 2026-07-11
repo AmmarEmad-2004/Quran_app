177a6ab5873f3d2662ccd07db5bbc08b9128dbab
Mariam Ibrahim <neamamohammed649@gmail.com>
2026-07-11 12:28:43 +0300

    api service done

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,1 +1,19 @@
-class ApiService {}
+import 'package:dio/dio.dart';
+import 'package:quran_app/core/constants/key_constants.dart';
+
+class ApiService {
+  final Dio _dio;
+
+  ApiService(this._dio){
+    _dio.options = BaseOptions(
+      baseUrl: KeyConstants.baseUrl);
+  }
+
+  Future<dynamic> get({required String endPoint, Map<String, dynamic>? queryParameters}) async {
+    final response = await _dio.get(
+      endPoint,
+      queryParameters: queryParameters,
+    );
+    return response.data;
+  }
+}
